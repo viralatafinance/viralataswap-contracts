8c883e92f0d2fbbdf54e3979965db2e027b2b474
Tecnocao <83429660+alexreau@users.noreply.github.com>
2021-08-06 20:35:47 +0200

    Add AURO contract

---
+++ contracts/auro/ViralataSwapToken.sol
@@ -0,0 +1,96 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.2;
+
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/metatx/ERC2771Context.sol";
+
+contract ViralataSwapToken is ERC20, ERC20Permit, Pausable, AccessControl {
+    using SafeERC20 for IERC20;
+    using SafeMath for uint256;
+
+    bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    bytes32 public constant RESCUER_ROLE = keccak256("RESCUER_ROLE");
+    bytes32 public constant TAKE_FEE_ROLE = keccak256("TAKE_FEE_ROLE");
+    bytes32 public constant ROUTER_ROLE = keccak256("ROUTER_ROLE");
+
+    uint256 private _cap = 2000000000 * 10**decimals(); // 2 billion tokens is maximum supply
+    uint256 private _initialSupply = 200000 * 10**decimals(); // 200,000 tokens is the initial supply
+
+    address private _trustedForwarder;
+
+    event TokensRescued(address indexed sender, address indexed token, uint256 value);
+
+    constructor(address trustedForwarder) ERC20("ViralataSwap Token", "AURO") ERC20Permit("ViralataSwap Token") {
+        _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
+        _setupRole(PAUSER_ROLE, _msgSender());
+        _setupRole(MINTER_ROLE, _msgSender());
+        _setupRole(RESCUER_ROLE, _msgSender());
+
+        _trustedForwarder = trustedForwarder;
+
+        _mint(_msgSender(), _initialSupply);
+    }
+
+    function isTrustedForwarder(address forwarder) public view virtual returns (bool) {
+        return forwarder == _trustedForwarder;
+    }
+
+    function _msgSender() internal view override returns (address sender) {
+        if (isTrustedForwarder(msg.sender)) {
+            // The assembly code is more direct than the Solidity version using `abi.decode`.
+            assembly {
+                sender := shr(96, calldataload(sub(calldatasize(), 20)))
+            }
+        } else {
+            return super._msgSender();
+        }
+    }
+
+    function _msgData() internal view override returns (bytes calldata) {
+        if (isTrustedForwarder(msg.sender)) {
+            return msg.data[:msg.data.length - 20];
+        } else {
+            return super._msgData();
+        }
+    }
+
+    /**
+     * @dev Returns the cap on the token's total supply.
+     */
+    function cap() public view returns (uint256) {
+        return _cap;
+    }
+
+    function pause() public onlyRole(PAUSER_ROLE) {
+        _pause();
+    }
+
+    function unpause() public onlyRole(PAUSER_ROLE) {
+        _unpause();
+    }
+
+    function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
+        _mint(to, amount);
+    }
+
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal override whenNotPaused {
+        super._beforeTokenTransfer(from, to, amount);
+    }
+
+    function rescueTokens(IERC20 token, uint256 value) external onlyRole(RESCUER_ROLE) {
+        token.transfer(_msgSender(), value);
+
+        emit TokensRescued(_msgSender(), address(token), value);
+    }
+}
